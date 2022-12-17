8452e45de7fdb8faa9211d1dea314aca0c6bfd28
KormazovaVer <98283035+KormazovaVer@users.noreply.github.com>
2022-12-17 20:18:20 +0300

    Update settings.py

--- orders/settings.py
+++ orders/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-&p#mv&w60p$)r&dbbcvk&601xe19eb%)q2-#n2v4-m)-n*871g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
