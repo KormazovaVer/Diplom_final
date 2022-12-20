caa9d12c4986f9525e6494b30a749645fecb460b
KormazovaVer <98283035+KormazovaVer@users.noreply.github.com>
2022-12-20 23:19:16 +0300

    Update forms.py

--- shops/forms.py
+++ shops/forms.py
@@ -3,7 +3,7 @@ from allauth.socialaccount.forms import SignupForm
 
 class UserSignupForm(SignupForm):
     def save(self, request):
-        user = super(SignupForm, self).save(request)
+        user = super(UserSignupForm, self).save(request)
         user.is_active = True
         user.save()
         return user
